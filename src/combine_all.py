18dd578bd9b73ff9825799a4649e67dd0b8408eb
Catherine <cawu@g.hmc.edu>
2026-03-09 22:26:06 -0700

    calculate routes

--- src/combine_all.py
+++ src/combine_all.py
@@ -6,13 +6,18 @@ stop_times = pd.read_csv("data_preprocessing/processed/all_stop_times.csv")
 stops = pd.read_csv("data_preprocessing/processed/all_stops.csv")
 
 # Join trips with stop_times
+stop_times['trip_id'] = stop_times['trip_id'].astype(str)
+trips['trip_id'] = trips['trip_id'].astype(str)
 trip_stop_times = pd.merge(stop_times, trips, on="trip_id", how="left")
 
 # Join the result with stops
+trip_stop_times['stop_id'] = trip_stop_times['stop_id'].astype(str)
+stops['stop_id'] = stops['stop_id'].astype(str)
 final_data = pd.merge(trip_stop_times, stops, on="stop_id", how="left")
 
 # Check result
 print(final_data.head())
+print("TEST",final_data[final_data['route_id']=='102619'])
 
 # Save if needed
-final_data.to_csv("joined_transit_data.csv", index=False)
+final_data.to_csv("./processed/joined_transit_data.csv", index=False)
--- src/pantry_map/data/loader.py
+++ src/pantry_map/data/loader.py
@@ -4,19 +4,19 @@ from pantry_map.utilities.utility import lat_lon_to_mercator, color_from_id
 from pantry_map.config import BASE_DIR
 
 def get_transit_df():
-    data_path = os.path.join(BASE_DIR, "pantry_map", "data", "raw", "shapes.txt")
+    data_path = os.path.join(BASE_DIR, "pantry_map", "data", "raw", "all_shapes.csv")
     transit_df = pd.read_csv(data_path)
     transit_df["x"], transit_df["y"] = lat_lon_to_mercator(
         transit_df["shape_pt_lat"].values,
         transit_df["shape_pt_lon"].values
     )
     grouped_df = transit_df.groupby("shape_id").agg({"x": list, "y": list}).reset_index()
-    grouped_df['color'] = grouped_df['shape_id'].apply(color_from_id)
+    grouped_df['color'] = grouped_df['shape_id'].apply(color_from_id) # TODO: ungroup, each segment is a line. colors are grouped.
     return grouped_df
 
 def get_foodbank_df():
     data_path = os.path.join(BASE_DIR, "pantry_map", "data", "raw",
-                             "Emergency_Food_and_Meals_Seattle_and_King_County_20260222.csv")
+                             "clean_food_resources.csv")
     foodbank_df = pd.read_csv(data_path)
     foodbank_df["x"], foodbank_df["y"] = lat_lon_to_mercator(
         foodbank_df["Latitude"].values,
